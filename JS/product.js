fe5a1cd12dfd56c6148b828dd0d979c881683f71
krystal-lulu <lanou@YF11-11.local>
2016-12-16 16:31:24 +0800

    修改

--- JS/product.js
+++ JS/product.js
@@ -6,13 +6,16 @@ $(function(){
         url:'product.php',
         dataType:"json",
         success:function(json){
-            console.log(typeof (json))
-            // $('.ProductLis').append(
-            //    '<div class="ProductLi">'
-            //          + '<img src="img/product0.jpg" width="100%"/>'
-            //    +'</div>'
-            //    +' <div class="ProductName">3 IN 1 CONVERTIBLE PLAYMAT WIHT LIGHT,MUSIC,INCLUDE 2AA BATTERIES</div>'
-            // );
+            for(var i=0;i<json.length;i++){
+                // $('.ProductLis').append(
+                //    '<div class="ProductLi">'
+                //          + '<img src="img/product0.jpg" width="100%"/>'
+                //    +'</div>'
+                //    +' <div class="ProductName">3 IN 1 CONVERTIBLE PLAYMAT WIHT LIGHT,MUSIC,INCLUDE 2AA BATTERIES</div>'
+                // );
+            }
+            console.log(json[0])
+
 
         }
 
--- product.php
+++ product.php
@@ -5,5 +5,5 @@
     $sql1='SELECT * FROM product';
     $result=mysqli_query($conn,$sql1);
     $result = json_encode(mysqli_fetch_all($result,MYSQLI_ASSOC));
-    echo '{"id":"12","type":"arrival","intro":"Lightweight Foldable Easy Carry Baby Kids Stroller","imageUrl":"product1.jpg"}';
+    echo $result;
 ?>
