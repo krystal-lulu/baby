3356fe7ceee7b93356d1e8e4203b9fccc994a144
krystal-lulu <lanou@YF11-11.local>
2016-12-19 13:58:11 +0800

    最新

--- PHP/product.php
+++ PHP/product.php
@@ -1,11 +1,12 @@
 <?php
     include('common.php');
     $num=$_GET['num'];
+    $type=$_GET['type'];
     $start=($num-1)*size;
     if($_GET['type']==''){
         $sql1="SELECT * FROM product LIMIT $start".','.size;
     }else{
-        $sql1="SELECT * FROM product WHERE type='feed' LIMIT $start".','.size;
+        $sql1="SELECT * FROM product WHERE type='".$type."'LIMIT $start".','.size;
     }
     $result1=mysqli_query($conn,$sql1);
     $result1 =json_encode(mysqli_fetch_all($result1,MYSQLI_ASSOC));
